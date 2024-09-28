cc15f29c07ec4d8abdeb9cb29a3a8e28c71a0500
DDR23 <roboreserva.andre@gmail.com>
2024-09-28 16:00:53 -0300

    save review

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
-# DNC-atividade-landingpage
-Atividade propostos durante o curso de Desenvolvimento WEB FullStack pela escola DNC
+# atividade---FRONTEND---landingpage-curso-tech---Escola-DNC
 
-## OBJETIVO
+## Sobre o projeto
 Site com informações, formulário de captura e redirecionamento para ementa da formção de Tecnologia da Escola DNC
 
-## Tecnologias utilizadas
--HTML
--CSS
--JAVASCRIPT
+Projeto hospedado em: https://dnc-atividade-landingpage.netlify.app/
 
-## Hospedado
-- [dnc-atividade-landingpage.netlify.app](https://dnc-atividade-landingpage.netlify.app)
-
-
-**Desenvolvido por DDR23 (André Campos)**
+## Screenshots
+![App Screenshot](/public/picture_01.png)
--- src/index.js
+++ src/index.js
@@ -4,21 +4,20 @@ var samantha = document.getElementById('depoimentosamantha')
 var bruna = document.getElementById('depoimentobruna')
 var setaDireita = document.getElementById('seta-direita')
 
-function RolarParaDireita(){
-    setaEsquerda.style.color = "#EAECE5"
-    setaEsquerda.style.cursor = "pointer"
-    leonardo.style = "display:none"
-    bruna.style = "display:flex"
-    setaDireita.style.color = "#247BA0"
-    setaDireita.style.cursor = "default"
+function RolarParaDireita() {
+  setaEsquerda.style.color = "#EAECE5"
+  setaEsquerda.style.cursor = "pointer"
+  leonardo.style = "display:none"
+  bruna.style = "display:flex"
+  setaDireita.style.color = "#247BA0"
+  setaDireita.style.cursor = "default"
 }
 
-function rolarParaEsquerda(){
-    setaEsquerda.style = "color: #247BA0"
-    setaEsquerda.style.cursor = "default"
-    leonardo.style = "display: flex"
-    bruna.style = "display: none"
-    setaDireita.style = "color #EAECE5"
-    setaDireita.style.cursor = "pointer"
+function rolarParaEsquerda() {
+  setaEsquerda.style = "color: #247BA0"
+  setaEsquerda.style.cursor = "default"
+  leonardo.style = "display: flex"
+  bruna.style = "display: none"
+  setaDireita.style = "color #EAECE5"
+  setaDireita.style.cursor = "pointer"
 }
-
